46387f1dfdcdd886b5dcf89fb4932306ba3282c3
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-01 14:50:34 +0300

    initial commit

--- test/heavyEx-api.test.js
+++ test/heavyEx-api.test.js
@@ -40,7 +40,7 @@ describe('when initially there are some equipment', () => {
     });
 });
 
-describe('geting a single equipment', () => {
+describe('fetching a single equipment', () => {
     test('succeeds when given a valid id', async () => {
         let equpmentAtStart = await helper.equipmentInDb()
         const response = await api.get(`/api/equipment/${equpmentAtStart[0].id}`)
@@ -54,9 +54,66 @@ describe('geting a single equipment', () => {
             .expect(404)
     })
 
-    test('fails with statuscode 400 when given an invalid id', async() => {
+    test('fails with statuscode 400 when given an invalid id', async () => {
         const response = await api.get(`/api/equipment/huhu6g`)
-        .expect(400)
+            .expect(400)
+    })
+})
+
+describe('addition of a new equipment', () => {
+    test('succeeds when given valid data', async () => {
+        let equpmentAtStart = await helper.equipmentInDb()
+        const response = await api.post('/api/equipment')
+            .send(helper.newEquipment)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+        let equpmentAtEnd = await helper.equipmentInDb()
+        expect(equpmentAtEnd).toHaveLength(equpmentAtStart.length + 1)
+    })
+
+    test('fails when given invalid data', async () => {
+        let equpmentAtStart = await helper.equipmentInDb()
+        const response = await api.post('/api/equipment')
+            .send(helper.InvalidEqupment)
+            .expect(400)
+        let equpmentAtEnd = await helper.equipmentInDb()
+        expect(equpmentAtEnd).toHaveLength(equpmentAtStart.length)
+    })
+})
+
+describe('updating a single equipment', () => {
+    test('succeeds when given a valid id and data', async () => {
+        let equpmentAtStart = await helper.equipmentInDb()
+        await api.put(`/api/equipment/${equpmentAtStart[0].id}`)
+            .send(helper.updateData)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        let equpmentAtEnd = await helper.equipmentInDb()
+        const names = equpmentAtEnd.map(e => e.name)
+        expect(names).toContain(helper.updateData.name)
+    })
+
+    test('fails with statuscode 404 when given a nonexistentId', async () => {
+        const nonExistentId = '6602c339544e74391a37202c'
+        const response = await api.put(`/api/equipment/${nonExistentId}`)
+            .send(helper.updateData)
+            .expect(404)
+    })
+
+    test('fails with statuscode 400 when given an invalid id', async () => {
+        const response = await api.put(`/api/equipment/huhu6g`)
+            .send(helper.updateData)
+            .expect(400)
+    })
+})
+
+describe('deleting a single equpment', () => {
+    test('succeeds when given a valid id', async () => {
+        let equpmentAtStart = await helper.equipmentInDb()
+        await api.delete(`/api/equipment/${equpmentAtStart[0].id}`)
+            .expect(204)
+        let equpmentAtEnd = await helper.equipmentInDb()
+        expect(equpmentAtEnd).toHaveLength(equpmentAtStart.length - 1)
     })
 })
 
