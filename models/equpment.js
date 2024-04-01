0eadac9ea8ea3bfec200995ba7109cc15ad2be8e
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-01 14:50:14 +0300

    initial commit

--- models/equpment.js
+++ models/equpment.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
 const equipemntSchema = new mongoose.Schema({
-    name: { type: String, require: true },
-    price: { type: Number, require: true },
-    description: { type: String, require: true },
-    type: { type: String, require: true },
-    imageUrl: { type: String, require: true },
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    type: { type: String, required: true },
+    imageUrl: { type: String, required: true },
 })
 
 equipemntSchema.set('toJSON', {
