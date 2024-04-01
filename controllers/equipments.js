0e4453706981835464f85d967a44bec47ad8628b
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-01 14:50:24 +0300

    initial commit

--- controllers/equipments.js
+++ controllers/equipments.js
@@ -24,13 +24,46 @@ equipmentRouter.get('/:id', async (request, response, next) => {
     }
 })
 
-// equipmentRouter.get('/', async (request, response, next) => {
-//     try {
-//         const equipment = await Equipment.find({})
-//         response.status(200).json(equipment)
-//     } catch (error) {
-//         next(error)
-//     }
-// })
+equipmentRouter.post('/', async (request, response, next) => {
+    const { name, price, description, type, imageUrl } = request.body
+    const equipmentObject = new Equipment({
+        name, price, description, type, imageUrl
+    })
+    try {
+        const savedEquipment = await equipmentObject.save()
+        response.status(200).json(savedEquipment)
+    } catch (error) {
+        next(error)
+    }
+})
+
+equipmentRouter.put('/:id', async (request, response, next) => {
+    const { name, price, description, type, imageUrl } = request.body
+
+    const data = { name, price, description, type, imageUrl }
+
+    try {
+        const equipment = await Equipment.findById(request.params.id)
+        if (!equipment) {
+            return response.sendStatus(404)
+        }
+        const updatedEquipment = await Equipment.findByIdAndUpdate(request.params.id,
+            data,
+            { new: true })
+
+        response.status(201).json(updatedEquipment)
+    } catch (error) {
+        next(error)
+    }
+})
+
+equipmentRouter.delete('/:id', async (request, response, next) => {
+    try {
+        await Equipment.findByIdAndDelete(request.params.id)
+        response.sendStatus(204)
+    } catch (error) {
+        next(error)
+    }
+})
 
 module.exports = equipmentRouter
